eb736f8a49cd8871f477895a6110a6839c4ce823
Krishna <Krishnathombare43@gmail.com>
2024-12-30 20:41:29 +0530

    Used Bootstrap Customization

--- app.py
+++ app.py
@@ -2,10 +2,39 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Cinematographer',
+        'location': 'Pune, India',
+        'salary': 'Rs. 70,000'
+    },
+    
+    {
+        'id': 2,
+        'title': 'Director',
+        'location': 'Pune, India',
+        'salary': 'Rs. 100,000'
+    },
+    
+    {
+        'id': 3,
+        'title': 'Writer',
+        'location': 'Pune, India',
+        'salary': 'Rs. 85,000'
+    },
+    
+    {
+        'id': 4,
+        'title': 'Editor',
+        'location': 'Pune, India',
+        'salary': 'Rs. 70,000'
+    }
+]
+
 @app.route('/')
 def home():
-    return render_template('index.html')
-
+    return render_template('index.html', jobs=JOBS, company_name='FINISHER')
 
 
 
