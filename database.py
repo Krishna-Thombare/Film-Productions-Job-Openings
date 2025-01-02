1cc3386a20fdaacda28d6fc491dec541a4b6b1a1
Krishna <Krishnathombare43@gmail.com>
2025-01-02 20:52:23 +0530

    Stored Application Data into the Database

--- database.py
+++ database.py
@@ -29,3 +29,28 @@ def load_job_from_db(id):
             return None
         else:
             return dict(rows[0]._mapping)
+        
+def add_application_to_db(job_id, data):
+    with engine.connect() as conn:
+        query = text(
+            "INSERT INTO applications("
+            "job_id, full_name, email, contact_no, portfolio_link, work_experience"
+            ") VALUES("
+            ":job_id, :full_name, :email, :contact_no, :portfolio_link, :work_experience"
+            ")"
+        )
+
+        params = {
+                'job_id': job_id,
+                'full_name': data['full_name'],
+                'email': data['email'],
+                'contact_no': data['contact_no'],
+                'portfolio_link': data['portfolio_link'],
+                'work_experience': data['work_experience']
+            }
+
+        # Execute query and commit
+        conn.execute(query, params)
+        conn.commit()  # Commit the transaction
+        print("Application added to database successfully.")
+    
