388b86ba5ef6360a5604de1efb64a50743c59194
Krishna <Krishnathombare43@gmail.com>
2025-01-03 19:25:57 +0530

    Updated Navigation Bar and Added New Pages.

--- app.py
+++ app.py
@@ -3,17 +3,29 @@ from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
+# Home Page
 @app.route('/')
 def home():
     jobs = load_jobs_from_db()
     return render_template('index.html', jobs=jobs)
 
+# About Us Page
+@app.route('/aboutus')
+def about_us():
+    return render_template('aboutus.html')
+
+# Get Update Page
+@app.route('/getupdates')
+def get_updates():
+    return render_template('getupdates.html')
+    
 # To store jobs information in json format
 @app.route('/api/jobs')       #api route
 def list_jobs():
     jobs = load_jobs_from_db()
     return jsonify(jobs)
 
+# Job Description Page
 @app.route('/job/<id>')
 def show_job(id):
     job = load_job_from_db(id)
@@ -22,6 +34,7 @@ def show_job(id):
         return "Not Found!", 404
     return render_template('jobpage.html', job=job)
 
+# Application Submitted Page
 @app.route('/job/<id>/apply', methods=['POST'])
 def apply_to_job(id):
     data = request.form
--- database.py
+++ database.py
@@ -14,7 +14,6 @@ def load_jobs_from_db():
         
         # Created list of dictionaries
         jobs = [dict(row._mapping) for row in result]
-        print(jobs)
         return jobs
 
 def load_job_from_db(id):
@@ -23,7 +22,6 @@ def load_job_from_db(id):
                 text("SELECT * FROM jobs WHERE id = :val "),
                 {"val" : id}
         )
-
         rows = result.all()
         if len(rows) == 0: 
             return None
@@ -34,9 +32,9 @@ def add_application_to_db(job_id, data):
     with engine.connect() as conn:
         query = text(
             "INSERT INTO applications("
-            "job_id, full_name, email, contact_no, portfolio_link, work_experience"
+            "job_id, full_name, email, contact_no, address, portfolio_link, work_experience"
             ") VALUES("
-            ":job_id, :full_name, :email, :contact_no, :portfolio_link, :work_experience"
+            ":job_id, :full_name, :email, :contact_no, :address, :portfolio_link, :work_experience"
             ")"
         )
 
@@ -45,6 +43,7 @@ def add_application_to_db(job_id, data):
                 'full_name': data['full_name'],
                 'email': data['email'],
                 'contact_no': data['contact_no'],
+                'address': data['address'],
                 'portfolio_link': data['portfolio_link'],
                 'work_experience': data['work_experience']
             }
